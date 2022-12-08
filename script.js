33f68dbd3d1e6aeca6628e5e7b6235a29fd468ea
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2022-12-08 07:08:20 +0530

    adding additional functionality of adding new field

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 
+function addNewWeField(){
+
+   let newNode=document.createElement('textarea');
+   newNode.classList.add('form-control');
+   newNode.classList.add('weField');
+
+   newNode.classList.add("mt-2");
+
+   newNode.setAttribute('rows',3);
+   newNode.setAttribute("placeholder","Enter here");
+
+   let weOb=document.getElementById("we");
+   let weAddButtonOb=document.getElementById("weAddButton");
+
+   weOb.insertBefore(newNode,weAddButtonOb);
+}
+
+
+function addNewaqField(){
+
+   let newNode=document.createElement('textarea');
+   newNode.classList.add('form-control');
+   newNode.classList.add('eqField');
+
+   newNode.classList.add("mt-2");
+
+   newNode.setAttribute('rows',3);
+   newNode.setAttribute("placeholder","Enter here");
+
+   let aqOb=document.getElementById("aq");
+   let aqAddButtonOb=document.getElementById("aqAddButton");
+
+   aqOb.insertBefore(newNode,aqAddButtonOb);
+}
 
 
 
